13a33b3f541f6b748e5d0f5d72875e11d5ca5d86
tubabayik <tubabayik@gmail.com>
2015-05-14 13:30:16 -0700

    Update Sample.cpp

--- CmdlineParser/Sample.cpp
+++ CmdlineParser/Sample.cpp
@@ -3,10 +3,9 @@ int main(int argc, const char* argv[])
     int op1, op2;
 
     CmdlineOpts opts = {
-                     
-                                { &op1, "o1", "op1", ARG_MANDATORY, "does this and that..." },
-                                { &op2, "o2", "op2", ARG_OPTIONAL, "does something else" },
-                                CmdlineArgGroup{
+                            { &op1, "o1", "op1", ARG_MANDATORY, "does this and that..." },
+                            { &op2, "o2", "op2", ARG_OPTIONAL, "does something else" },
+                            CmdlineArgGroup{
                                     { &op1, "o5", "op1", ARG_MANDATORY, "does a lot..." },
                                     { &op2, "o2", "op2", ARG_MANDATORY }
                             }
